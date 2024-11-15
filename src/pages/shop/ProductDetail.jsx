e3cb177e5e0fbf87815dc02d8cd892e25315c185
berryicebox <feelmypasta@gmail.com>
2024-11-15 15:05:22 +0900

    design: 상품 상세 네비게이션 디자인 적용

---
+++ src/pages/shop/ProductDetail.jsx
@@ -0,0 +1,14 @@
+import MainDetail from "../../components/shop/MainDetail";
+import ProductDetailNav from "../../components/shop/ProductDetailNav";
+
+const ProductDetail = () => {
+
+    return(
+        <>
+            <MainDetail/>
+            <ProductDetailNav/>
+        </>
+    )
+}
+
+export default ProductDetail;
