3e1e1199216abcc0dec69a23a81e5cdf8a4ebc25
berryicebox <berryicebox@github.com>
2024-11-25 16:47:41 +0900

    feat: 옵션 2개 이상일 때 장바구니 -> 옵션선택으로 변경

--- src/components/shop/product/Product.jsx
+++ src/components/shop/product/Product.jsx
@@ -1,50 +1,45 @@
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faStar} from "@fortawesome/free-solid-svg-icons";
 import {Link} from "react-router-dom";
+import instance from "../../../utils/axios";
 
 const Product = (props) => {
 
-    console.log("product 데이터 테스트: ", props.data); // 
-
     let cart = [];
+    const optionCount = props.data?.option_count;
+    let storageCart = localStorage.getItem("cart");
 
-    // const handlePostCart = async () => {
 
-    //     const data = {
-    //         "itemId" : props.data.id
-    //         "count" : 3, // 이건 생각해 봐야함
-    //         "optionId" : 37 
-    //     }
+    const handlePostCart = async () => {
 
-    //     console.log(data)
+        let baseData = localStorage.getItem("cart"); // 장바구니 데이터
+        baseData = JSON.parse(storageCart);
+        console.log("handlePostCart baseData: ", baseData);
+        const data = baseData[0];
+        console.log(data);
 
-    //     try {
-    //         const response = await instance({
-    //             url: "/cart/add",
-    //             method: "post",
-    //             data: data
-    //         });
+        try {
+            const response = await instance({
+                url: "/cart/add",
+                method: "post",
+                data: data
+            });
 
-    //         // 성공적으로 데이터가 저장된 경우
-    //         console.log('등록 성공:', response.data);
+            // 성공적으로 데이터가 저장된 경우
+            console.log('장바구니 데이터 등록 성공:', response.data);
 
-    //     } catch (error) {
-    //         // 에러가 발생한 경우
-    //         console.log('에러 발생:', error);
-    //     }
-    // }
+        } catch (error) {
+            // 에러가 발생한 경우
+            console.log('장바구니 데이터 에러 발생:', error);
+        }
+    }
 
 
     const addCart = () => {
         const item = {
-            title: props.data?.name,
-            count: 1 };
-
-        const test = {
-            itemId : 18,
-            count : 3,
-            opitonId : 37
-        }
+            itemId: props.data?.id,
+            count: 1,
+        };
 
         let storageCart = localStorage.getItem("cart");
 
@@ -53,7 +48,7 @@ const Product = (props) => {
 
             storageCart = JSON.parse(storageCart);
             storageCart.every(cartItem=>{
-                     if (cartItem.title === item.title) {
+                     if (cartItem.itemId === item.itemId) {
                         cartItem.count = cartItem.count+1;
                          isExist = true;
                         return false;
@@ -79,7 +74,6 @@ const Product = (props) => {
 
     return(
 
-        <Link to={`http://localhost:3000/shop/detail/${props.data?.id}`}>
           <div className={props.position}>
 
               <img src={props?.data["thumbnail_url"]} alt=""/>
@@ -87,21 +81,35 @@ const Product = (props) => {
                   <span className="brand">{props.data?.nickname}</span>
                   <span className="title">{props.data?.name}</span>
                   <span className="price">{props.data?.price}원</span>
+
                   <div className="option">
                       <span className="option">참치맛</span>
                       <hr className="vertical"/>
                       <span className="count">{props.data?.count}</span>
                   </div>
+
                   <span className="star"><FontAwesomeIcon icon={faStar}/>{props.data?.rating}</span>
-                  {props.position==="product"?
-                      <button onClick={addCart} className="cart-button">장바구니</button>:null}
-                  <Link to={`http://localhost:3000/shop/detail/${props.data?.id}`}>
-                     <button>옵션선택</button>
-                  </Link>
+                  {props.position==="product" && (
+                    <div>
+                        {optionCount > 1 ?
+                            (<Link to={`http://localhost:3000/shop/detail/${props.data?.id}`}>
+                                <button className="cart-button">옵션선택</button>
+                            </Link>) : 
+                            ( <button 
+                                onClick={() => {
+                                    addCart();
+                                    handlePostCart();}
+                                } 
+                                className="cart-button">
+                                    장바구니
+                              </button> )
+                        }                        
+                    </div>
+                  )}
               </div>
 
           </div>
-        </Link>
       )
 }
-export default Product
+
+export default Product;
