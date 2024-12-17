c010652e14c8c34883334453a928ecb2ff3b1238
berryicebox <berryicebox@github.com>
2024-12-17 16:07:59 +0900

    fix: 상품 상세 할인 표시 예외 추가 및 상품 후기 별점 표시 동적으로 적용
    상품 상세 할인 표시 예외 추가
    상품 후기 별점 표시 동적으로 적용

--- src/components/shop/product/review/ProductReview.jsx
+++ src/components/shop/product/review/ProductReview.jsx
@@ -2,11 +2,13 @@ import "../../../../assets/styles/shop/product/productReview.scss"
 import instance from "../../../../utils/axios";
 import {useEffect, useState} from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faHeart, faHeartBroken} from "@fortawesome/free-solid-svg-icons";
+import {faHeart, faStar, faHeartBroken} from "@fortawesome/free-solid-svg-icons";
 import ReviewImages from "./ReviewImages";
 import {useModifyTime} from "../../../../utils/useModifyTime";
 
 const ProductReview = ({ isLoggedIn ,item, setIsModified, isModified, setIsEdit, isEdit}) => {
+
+    console.log("ProductReview item", item);
 
     const [isAuthor, setIsAuthor] = useState()
     const [isLiked, setIsLiked] = useState(item.heart)
@@ -83,6 +85,12 @@ const ProductReview = ({ isLoggedIn ,item, setIsModified, isModified, setIsEdit,
         })
     }
 
+    const renderStars = (rating) => {
+        return Array.from({length: 5}, (_, index) => (
+            <span key={index}>{index < rating ? '★' : ''}</span>
+        ));
+    };
+
     return (
         <div className="productReviewContainer">
             <div className="reviewerInfoContainer">
@@ -90,10 +98,15 @@ const ProductReview = ({ isLoggedIn ,item, setIsModified, isModified, setIsEdit,
                 <div className="reviewerInfoDetail">
                     <div className="reviewerStars">
                         <div>
-                            {/*todo 별 숫자 -> 갯수로 바꾸기*/}
-                            <span>★</span><span>★</span><span>★</span><span>★</span><span>★</span>
+                            {[...Array(5)].map((_, i) => {
+                                return (
+                                    <span key={i} className={i < item.rating ? "selected" : "non-selected"}>
+                                        <FontAwesomeIcon icon={faStar}/>
+                                    </span>
+                                )
+                            })}
                         </div>
-                        <p>5</p>
+                        <p>{item.rating}</p>
                     </div>
                     <div className="review-header">
                         <div className="reviewerName">
