fa1429e98fe3062a8613344f5a72aa8f86c76e15
berryicebox <berryicebox@github.com>
2024-12-23 15:25:15 +0900

    design: 유기동물 상단 필터 테스트 레이아웃

--- src/components/additional/adopt/AdoptFilterMax.jsx
+++ src/components/additional/adopt/AdoptFilterMax.jsx
@@ -42,101 +42,101 @@ const AdoptFilterMax = (props) => {
     }
 
     return (
-        <>
-        <Card className="default-card filter-max-container">
-            <div className="kind-selector">
-                <Card>
-                    <button onClick={() => {
-                        setIsCat(false);
-                        getRefreshData();
-                        handleAddSpecies("개");
-                    }}>
-                        강아지
-                    </button>
-                </Card>
-                <Card>
-                    <button onClick={() => {
-                        setIsCat(true);
-                        getRefreshData();
-                        handleAddSpecies("고양이");
-                    }}>
-                        고양이
-                    </button>
-                </Card>
-            </div>
-            <div className="breed-selector">
-                <p onClick={() => {setClick(prevState => ({
-                        ...prevState,
-                        breed: !prevState.breed}))
-                }}>
-                    {selectedItems.breed?.length > 0 
-                        ? selectedItems.breed?.length === 1 ?
-                            `${selectedItems.breed[0].name}` :
-                            `${selectedItems.breed[0].name} 외 ${selectedItems.breed?.length - 1}건`
-                        : "품종 선택"}
-                </p>
-                {isCat ?
-                    <Filter
-                        className="breed-filter"
-                        placeholder="품종 검색"
-                        array={catBreedSelector}
-                        isClick={click.breed}
-                        selectedItems={selectedItems}
-                        setSelectedItems={setSelectedItems}
-                        getRefreshData={getRefreshData}
-                    /> :                    
-                    <Filter
-                        className="breed-filter"
-                        placeholder="품종 검색"
-                        array={dogBreedSelector}
-                        isClick={click.breed}
-                        selectedItems={selectedItems}
-                        setSelectedItems={setSelectedItems}
-                        getRefreshData={getRefreshData}
-                    />                
-                }
-            </div>
-            <div className="region-selector">
-                <p onClick={() => {setClick(prevState => ({
-                            ...prevState,
-                            region: !prevState.region}))
-                    }}>
-                    {selectedItems.location?.length > 0 
-                        ? selectedItems.location[1] ? 
-                            `${selectedItems.location[0]} ${" " + selectedItems.location[1]}` : 
-                            `${selectedItems.location[0]}`
-                        : "지역 선택"}
-                </p>
-                <FilterRegion
-                    className="region-filter"
-                    array={regionOptions}
-                    isClick={click.region}
-                    selectedItems={selectedItems}
-                    setSelectedItems={setSelectedItems}
-                    getRefreshData={getRefreshData}
-                />
-            </div>
-        </Card>
-        <div className="filter-breed-container">
-            {selectedItems.breed?.length > 0 && (
-            <>
-                <div className="filter-breed-list">
-                    {selectedItems.breed.map((item, index) => {
-                        
-                        return (
-                            <div className="filter-breed-item" key={item.key}>
-                                <span>{item.name}</span>
-                                <button onClick={() => handleDeleteItem(index)}>X</button>                
-                            </div>
-                        )
-
-                    })}
+        <div className="filter-container">
+            <Card className="default-card filter-max-container">
+                <div className="kind-selector">
+                    <Card>
+                        <button onClick={() => {
+                            setIsCat(false);
+                            getRefreshData();
+                            handleAddSpecies("개");
+                        }}>
+                            강아지
+                        </button>
+                    </Card>
+                    <Card>
+                        <button onClick={() => {
+                            setIsCat(true);
+                            getRefreshData();
+                            handleAddSpecies("고양이");
+                        }}>
+                            고양이
+                        </button>
+                    </Card>
                 </div>
-                <button onClick={() => setSelectedItems([])}>선택 초기화</button>
-            </>
-            )}
+                <div className="breed-selector">
+                    <p onClick={() => {setClick(prevState => ({
+                            ...prevState,
+                            breed: !prevState.breed}))
+                    }}>
+                        {selectedItems.breed?.length > 0 
+                            ? selectedItems.breed?.length === 1 ?
+                                `${selectedItems.breed[0].name}` :
+                                `${selectedItems.breed[0].name} 외 ${selectedItems.breed?.length - 1}건`
+                            : "품종 선택"}
+                    </p>
+                    {isCat ?
+                        <Filter
+                            className="breed-filter"
+                            placeholder="품종 검색"
+                            array={catBreedSelector}
+                            isClick={click.breed}
+                            selectedItems={selectedItems}
+                            setSelectedItems={setSelectedItems}
+                            getRefreshData={getRefreshData}
+                        /> :                    
+                        <Filter
+                            className="breed-filter"
+                            placeholder="품종 검색"
+                            array={dogBreedSelector}
+                            isClick={click.breed}
+                            selectedItems={selectedItems}
+                            setSelectedItems={setSelectedItems}
+                            getRefreshData={getRefreshData}
+                        />                
+                    }
+                </div>
+                <div className="region-selector">
+                    <p onClick={() => {setClick(prevState => ({
+                                ...prevState,
+                                region: !prevState.region}))
+                        }}>
+                        {selectedItems.location?.length > 0 
+                            ? selectedItems.location[1] ? 
+                                `${selectedItems.location[0]} ${" " + selectedItems.location[1]}` : 
+                                `${selectedItems.location[0]}`
+                            : "지역 선택"}
+                    </p>
+                    <FilterRegion
+                        className="region-filter"
+                        array={regionOptions}
+                        isClick={click.region}
+                        selectedItems={selectedItems}
+                        setSelectedItems={setSelectedItems}
+                        getRefreshData={getRefreshData}
+                    />
+                </div>
+            </Card>
+            <div className="filter-breed-container">
+                {selectedItems.breed?.length > 0 && (
+                <>
+                    <div className="filter-breed-list">
+                        {selectedItems.breed.map((item, index) => {
+                            
+                            return (
+                                <div className="filter-breed-item" key={item.key}>
+                                    <span>{item.name}</span>
+                                    <button onClick={() => handleDeleteItem(index)}>X</button>                
+                                </div>
+                            )
+
+                        })}
+                    </div>
+                    <button onClick={() => setSelectedItems([])}>선택 초기화</button>
+                </>
+                )}
+            </div>
         </div>
-        </>
     )
 }
 
